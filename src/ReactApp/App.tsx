771332211e7f981014b602cbad36e7861c53a018
NJ <nj.namju@gmail.com>
2018-12-21 14:37:03 -0800

    update

---
+++ src/ReactApp/App.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {Application } from '../Application';
+
+class App extends Component {
+  constructor(props: {}){
+    super(props);
+    const app = new Application();
+  }
+
+  render() {
+    return (
+      <div className="main">
+
+      </div>
+    );
+  }
+}
+
+export default App;
