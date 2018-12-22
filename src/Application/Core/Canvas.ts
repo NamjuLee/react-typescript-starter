3d5c843666aa7c2e4b1d1c8fc2820be554ed09c8
NJ <nj.namju@gmail.com>
2018-12-21 21:37:43 -0800

    update vis core

---
+++ src/Application/Core/Canvas.ts
@@ -0,0 +1,8 @@
+import { Application } from '../';
+
+export class Canvas {
+    app: Application;
+    constructor(app: Application) {
+        this.app = app;
+    }
+}
--- src/Application/index.ts
+++ src/Application/index.ts
@@ -1,13 +1,17 @@
-
+import { Canvas } from './Core/Canvas';
 export class Application {
 
     public host: HTMLElement | undefined;
+
+    canvas: Canvas;
 
     constructor(id: string) {
         console.log('id: ' + id + ', App Init!!!!!');
 
         const host = document.getElementById(id);
         if (host) {this.host = host; }
-        console.log(this.host);
+        
+        this.canvas = new Canvas(this);
+        console.log(this.canvas);
     }
 }
